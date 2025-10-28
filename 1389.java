654cb5f6d6911ab248a1dd20d86c391d8c89a063
Shashank <shashankbyali@gmail.com>
2025-10-28 18:42:51 +0530

    Update 1389.java

--- 1389.java
+++ 1389.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            list.add(index[i],nums[i]);
+        }
         int[] target = new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            int temp=target[index[i]];
-            target[index[i]]=nums[i];
-            target[index[i]+1]=temp;
-            // target[index[i]]=nums[i];
+            target[i]= list.get(i);
         }
         return target;
     }
